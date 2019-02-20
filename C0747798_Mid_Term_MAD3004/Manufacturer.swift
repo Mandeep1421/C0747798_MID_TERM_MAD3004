2b658f7b129597d5d11ac34a83c3db3db8dce3a1
MacStudent <macstudent@126-imac16.local>
2019-02-20 18:30:15 -0500

    Manufacturer, Product, Orders and Display classes are created

---
+++ C0747798_Mid_Term_MAD3004/Manufacturer.swift
@@ -0,0 +1,9 @@
+//
+//  Manufacturer.swift
+//  C0747798_Mid_Term_MAD3004
+//
+//  Created by MacStudent on 2019-02-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
